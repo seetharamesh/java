c3bb633638c06826195fb541af707043d6786835
seetharamesh <seetharamesh@hotmail.com>
2021-04-15 23:27:04 -0400

    recursion and stack

--- src/javaSE/FibonacciRecursion.java
+++ src/javaSE/FibonacciRecursion.java
@@ -10,21 +10,22 @@ public class FibonacciRecursion {
 }
 	
 	static int fibonacci(int n) {	
-		if(n == 0) return n;
-			if(n > 0) {	
-			System.out.print(n1 + " " + n2 + " ");
+		if(n == 0 || n == 1) return n;
+		else
+//			if(n > 0) {	
+			//System.out.print(n1 + " " + n2 + " ");
 
-			n3 = n1 + n2;
-			n1=n2;
-			n2=n3;
-			n--;
-			System.out.println(n3);
-			fibonacci(n);
+//			n3 = n1 + n2;
+//			n1=n2;
+//			n2=n3;
+//			n--;
+			//System.out.println(n3);
+			n3 = fibonacci(n-1) + fibonacci(n-2);
 			return n3;			
-		}
-		else {
-			return 0;
-		}
+//		}
+//		else {
+//			return 0;
+//		}
 				
 //		without recursion	
 //		int n1=0, n2 = 1,n3=0;
@@ -37,8 +38,8 @@ public class FibonacciRecursion {
 	}
 
 	public static void main(String[] args) {
-		int result = fibonacci(10);
-		//System.out.println(result);
+		int result = fibonacci(6);
+		System.out.println(result);
 	}
 
 }
